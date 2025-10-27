107cc9917cd88e4f693225464b9b4fd263842a93
Manvesh-P <nikhilmanvesh@gmail.com>
2025-10-27 15:10:15 +0530

    local-settings

---
+++ local_settings.py
@@ -0,0 +1,10 @@
+from collections import OrderedDict
+
+
+db_settings = OrderedDict(
+    [('user', 'nikhilmanvesh'), 
+    ('password', 'manvesh123'), 
+    ('host', 'localhost'), 
+    ('port', '5432'), 
+    ('database', 'GlobalLinkLLC_Development')]
+)
